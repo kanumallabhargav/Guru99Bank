7c2ffd306d7c75c95429b2fa1bf8759962615b6c
munna <kanumallabhargav@gmail.com>
2020-06-16 00:50:08 +0530

    Optimization+Logging

--- src/test/java/bankAut/Guru99Bank_ext/SS1_Login.java
+++ src/test/java/bankAut/Guru99Bank_ext/SS1_Login.java
@@ -1,18 +1,22 @@
 package bankAut.Guru99Bank_ext;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
 import java.util.concurrent.TimeUnit;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.By;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.DataProvider;
-import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 import resources.parameters;
 
 public class SS1_Login extends Base
 {
-		@Parameters({"URL"})
+	private static Logger log = LogManager.getLogger(Base.class.getName());
 		@Test(dataProvider="DataProviderMethod")
 		public void test1(String usrNameFromDataProvider, String passWordFromDataProvider)
 		{
@@ -20,37 +24,47 @@ public class SS1_Login extends Base
 			driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 			parameters parametersClassObject =new parameters(driver);
 			driver.get(parametersClassObject.url);
-			System.out.println(usrNameFromDataProvider);
-			System.out.println(passWordFromDataProvider);
 			parametersClassObject.findUserNameBox().sendKeys(usrNameFromDataProvider);
 			parametersClassObject.findPasswordBox().sendKeys(passWordFromDataProvider);
 			parametersClassObject.findLoginButton().click();
 			if(driver.getTitle().equalsIgnoreCase("Guru99 Bank Manager HomePage"))
 			{
-				System.out.println("Title verified.");
-				System.out.println("Test Case Passed");
+				log.info("Title verified.");
+				log.info("Test Case Passed");
+			}
+			if(driver.findElement(By.linkText("Manager")).isDisplayed())
+			{
+				log.info("Login Success");
 			}
 			else
 			{
-				System.out.println("Title doesn't match.");
-				System.out.println("Test Case Failed");
+				log.error("login Failed");
 			}
-			System.out.println("down the line check");
-			driver.manage().deleteAllCookies();
-			driver.close();
 		}
 		@DataProvider
-		public Object[][] DataProviderMethod()
+		public Object[][] DataProviderMethod() throws IOException
 		{
+			FileInputStream fis = new FileInputStream("/Users/bhargavkanmalla/Documents/workspace/Guru99Bank_ext/src/main/java/resources/Data.properties");
+			Properties prop = new Properties();
+			prop.load(fis);
+			String validUID = prop.getProperty("validUserId");
+			String validPWD = prop.getProperty("validPassword");
+			String invalidData = prop.getProperty("invalidData");
 			Object[][] dataProviderObject = new Object[4][2];
-			dataProviderObject[0][0] = "mngr263563";
-			dataProviderObject[0][1] = "azAmEha";
-			dataProviderObject[1][0] = "INVALID";
-			dataProviderObject[1][1] = "azAmEha";
-			dataProviderObject[2][0] = "mngr263563";
-			dataProviderObject[2][1] = "INVALID";
-			dataProviderObject[3][0] = "INVALID";
-			dataProviderObject[3][1] = "INVALID";
+			dataProviderObject[0][0] = validUID;
+			dataProviderObject[0][1] = validPWD;
+			dataProviderObject[1][0] = invalidData;
+			dataProviderObject[1][1] = validPWD;
+			dataProviderObject[2][0] = validUID;
+			dataProviderObject[2][1] = invalidData;
+			dataProviderObject[3][0] = invalidData;
+			dataProviderObject[3][1] = invalidData;
 			return dataProviderObject;
+		}
+		@AfterTest
+		public void closeDriverAndClearCookies()
+		{
+			driver.manage().deleteAllCookies();
+			driver.close();
 		}
 }
